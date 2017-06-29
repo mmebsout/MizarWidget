fa595edc6e502b590e1b4ebb5325b1587aeedf6b
Ratamiez <ratamiez@gmail.com>
2017-06-29 09:33:02 +0200

    #34

--- src/main.js
+++ src/main.js
@@ -1,20 +1,20 @@
 /*******************************************************************************
  * Copyright 2012-2015 CNES - CENTRE NATIONAL d'ETUDES SPATIALES
  *
- * This file is part of SITools2.
+ * This file is part of MIZAR.
  *
- * SITools2 is free software: you can redistribute it and/or modify
+ * MIZAR is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * SITools2 is distributed in the hope that it will be useful,
+ * MIZAR is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with SITools2. If not, see <http://www.gnu.org/licenses/>.
+ * along with MIZAR. If not, see <http://www.gnu.org/licenses/>.
  ******************************************************************************/
 
 /**
@@ -254,13 +254,13 @@ require(["./MizarWidget"], function (MizarWidget) {
                 }
             }
         ]
-        
-        
+
+
 
     };
 
     var options = {
-        
+
         guiActivated: true,
         isMobile: false,
         mode: "Sky",
@@ -276,7 +276,7 @@ require(["./MizarWidget"], function (MizarWidget) {
         },
         elevationTracker: {
             "position": "bottom"
-        },        
+        },
         stats: {
             visible: true
         },
